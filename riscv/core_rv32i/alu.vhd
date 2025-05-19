442b5125d6b7189bb3c3eafb8a9fd6ccc42ac37a
Mônica Emediato <emediatomonica2@gmail.com>
2025-05-19 19:00:26 -0300

    Update alu.vhd

--- riscv/core_rv32i/alu.vhd
+++ riscv/core_rv32i/alu.vhd
@@ -7,32 +7,44 @@ entity alu is
 		op2:		in std_logic_vector(31 downto 0);
 		alu_op:		in std_logic_vector(3 downto 0);
 		result:		out std_logic_vector(31 downto 0);
-		zero:		out std_logic;
-		less_than:	out std_logic
-	);
+		zero:		out std_logic; -- coloca 1 quando a operacao resultante for 0, usado no BRANCH EQUAL
+		less_than:	out std_logic -- quando a entrada A for menor que a B, usada no BRANCH LESS THAN
+	     );
 end alu;
 
 architecture arch_alu of alu is
 	signal r, shift: std_logic_vector(31 downto 0);
 	signal shift_op2: std_logic_vector(4 downto 0);
-	signal addsub: std_logic_vector(32 downto 0);
+	signal addsub: std_logic_vector(32 downto 0); 
+	-- bit de sinal fica no bit 31
+	-- bit 33 :
 	signal less, left, logical: std_logic;
 begin
 	process(op1, op2, alu_op, addsub, less, shift_op2, shift)
 	begin
 		case alu_op is
-			when "0000" => r <= op1 and op2;
-			when "0001" => r <= op1 or op2;
-			when "0010" => r <= op1 xor op2;
-			when "0100" | "0101" => r <= addsub(31 downto 0);
-			when "0110" => r <= op2;
-			when "0111" | "1000" => r <= x"0000000" & "000" & less;
+			-- valores vem do tipo de operacao que vai realizar
+			when "0000" => r <= op1 and op2; -- AND
+			when "0001" => r <= op1 or op2; -- OR
+			when "0010" => r <= op1 xor op2; -- XOR
+			when "0100" | "0101" => r <= addsub(31 downto 0); -- ADD/SUB - Adição ("0100") ou Subtração ("0101")
+			when "0110" => r <= op2; -- passa direto 			-- LOAD IMEDIATO, pega valor do registrador e joga na ALU , o dado está na p isntrucao
+			--  MOV/LUI - Move valor ou carrega imediato na parte superior (Load Upper Immediate)
+			when "0111" | "1000" => r <= x"0000000" & "000" & less; -- "0111" / "1000"r <= x"0000000" & "000" & less
+			-- SLT/SLTU - Set Less Than (comparações com ou sem sinal)other
+			-- rsr <= shift
 			when others => r <= shift;
+			-- SLL/SRL/SRA - Deslocamentos lógicos à esquerda/direita ou aritmético à direi
+
 		end case;
 	end process;
 
 	addsub <= ('0' & op1) - ('0' & op2) when alu_op > "0100" else ('0' & op1) + ('0' & op2);
-	less <= addsub(32) when op1(31) = op2(31) or alu_op = "1000" else op1(31);
+	
+	less <= addsub(32) when op1(31) = op2(31) or alu_op = "1000" else op1(31); 
+			-- verifica se os dois bits sao iguais, se forem 0s ou 1s 
+			-- 
+			
 	less_than <= less;
 	zero <= not (r(31) or r(30) or r(29) or r(28) or r(27) or r(26) or r(25) or r(24) or
 			r(23) or r(22) or r(21) or r(20) or r(19) or r(18) or r(17) or r(16) or
